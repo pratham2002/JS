fc8f8fb71aa09881e7c44052762a637ebbebf933
Pratham <agrawalpratham2002@gmail.com>
2021-12-29 21:44:38 +0530

    eval_29Dec

--- unit_4/react/eval_29dec/src/Components/ListItems.jsx
+++ unit_4/react/eval_29dec/src/Components/ListItems.jsx
@@ -1,6 +1,6 @@
 function ListItem({name,img,price,desc}){
     return(
-        <div style={{display:"flex",minWidth:"500px",gap:15,margin:"auto",width:"50%",marginBottom:15}}>
+        <div style={{display:"flex",border:"1px solid black",padding:15,minWidth:"500px",gap:15,margin:"auto",width:"50%",marginBottom:15}}>
             <img src={img} style={{height:"150px"}} alt={name} />
             <div style={{display:"flex",paddingLeft:5,flexDirection:"column"}}>
                 <h3>{name}</h3>
--- unit_4/react/eval_29dec/src/Pages/Home.jsx
+++ unit_4/react/eval_29dec/src/Pages/Home.jsx
@@ -3,17 +3,19 @@ import ListItem from "../Components/ListItems";
 
 function Home(){
     const [data,setData] = useState([])
+    const [isLoading,setLoading] = useState(true);
     useEffect(()=>(
          fetch('http://localhost:8000/products')
         .then(response => response.json())
-        .then(res=>setData(res))
+        .then(res=>setData(res),setLoading(false))
         .catch(error => console.error('Error:', error))
         ),[]
     )
     return(<>
         <h3>Home</h3>
+        {isLoading && <h3>Loading...</h3>}
         {data?.map((e)=>{
-            console.log(e);
+            // console.log(e);
             return <ListItem key={e.id} img={e.image} name={e.name} price={e.price} desc={e.description}/>
         })}
     </>
